7c477a2cda1c582ca1a9e92adbe4e0145320c7c2
Notarin <424c414e4b@gmail.com>
2020-04-18 21:54:08 -0400

    first run

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const token = require("./token.json")
+const config = require("./config.json")
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
+  if (msg.content.startsWith() === config.prefix) {
     msg.reply('Pong!');
   }
 });
 
-client.login('token');
+client.login('token.token');

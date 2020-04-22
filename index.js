b96ab0eca9165183bf1d8d13c350426e2115646f
Notarin <424c414e4b@gmail.com>
2020-04-22 19:34:00 -0400

    added say

--- index.js
+++ index.js
@@ -9,9 +9,10 @@ client.on('ready', () => {
 
 client.on('message', msg => {
   if (!msg.author.bot) {
-    if (msg.content.startsWith(config.prefix + "say")) {
-      var say = msg.content.replace(config.prefix + "say","");
-      console.log(say);
+    if (msg.content.startsWith(config.prefix + "say ")) {
+      var say = msg.content.replace(config.prefix + "say ","");
+      msg.delete(1);
+      msg.send(say);
     }
     if (msg.channel.id === config.reactchan) {
   if (msg.content.startsWith(config.prefix)) {

30771be2fff3d9b23c88ce08674b6614a344617d
Notarin <424c414e4b@gmail.com>
2020-04-23 13:27:19 -0400

    debug

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ client.on('message', msg => {
   if (msg.content.startsWith(config.prefix)) {
     var full = msg.content.substr(config.prefix.length);
     var command = msg.content.substr(0,msg.content.indexOf(' '));
+    console.log(command);
     var args = msg.content.substr(msg.content.indexOf(' ')+1);
     if (command == "say" && msg.author.id == config.owner) {
       msg.delete();

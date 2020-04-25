2edce7b44af5c23eab047d405e404d1b53d3896e
Notarin <424c414e4b@gmail.com>
2020-04-25 00:39:39 -0400

    specified channel

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ client.on('message', msg => {
   return;
   }
   if (command == "play") {
-    connection.play(ytdl("https://www.youtube.com/watch?v=dQw4w9WgXcQ"), { type: 'opus' });
+    msg.member.voice.channel.play(ytdl("https://www.youtube.com/watch?v=dQw4w9WgXcQ"), { type: 'opus' });
     return;
   }
   if (msg.channel.id == config.reactchan && config.react) {

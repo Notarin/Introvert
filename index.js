6ea7c0dbffb08e7167e9c0accc3b89b41a7346f1
Notarin <424c414e4b@gmail.com>
2020-04-25 22:38:43 -0400

    forgot to commit change

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ client.on('message', msg => {
     return;
   }
   if (command == "join") {
-    const connection = await msg.member.voice.channel.join();
+    const connection = msg.member.voice.channel.join();
     connection.play(ytdl("https://www.youtube.com/watch?v=dQw4w9WgXcQ"), { type: 'opus' });
     return;
   }
